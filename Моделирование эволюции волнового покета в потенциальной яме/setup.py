f432868f6777bc35a37e0bf9d0e702e976b3d9c8
KalinichevMaxim <146014299+KalinichevMaxim@users.noreply.github.com>
2024-11-26 20:56:54 +0300

    Create setup.py

---
+++ Моделирование эволюции волнового покета в потенциальной яме/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy
+import platform
+
+if platform.system() == 'Linux':
+    compilation_args = ['-fopenmp']
+    link_args = ['-fopenmp']
+elif platform.system() == 'Darwin':
+    compilation_args = ['-Xpreprocessor', '-fopenmp', '-lomp']
+    link_args = ['-lomp']
+
+setup(
+    ext_modules=cythonize(["temporal_shred.pyx"]),
+    include_dirs=[numpy.get_include()]
+)    
